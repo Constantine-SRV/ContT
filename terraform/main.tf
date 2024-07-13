5fa529a1158ef514b2afc881d4a3587b0b34433a
Constantine <84151879+Constantine-SRV@users.noreply.github.com>
2024-07-13 21:40:02 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,8 +11,7 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-# Создание VPC
-resource "aws_vpc" "vpc" {
+resource "aws_vpc" "simple_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
@@ -22,34 +21,31 @@ resource "aws_vpc" "vpc" {
   }
 }
 
-# Создание публичной подсети
-resource "aws_subnet" "subnet" {
-  vpc_id                  = aws_vpc.vpc.id
+resource "aws_subnet" "simple_subnet" {
+  vpc_id                  = aws_vpc.simple_vpc.id
   cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
   availability_zone       = "eu-north-1a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "simple_subnet"
   }
 }
 
-# Создание интернет-шлюза
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
+resource "aws_internet_gateway" "simple_igw" {
+  vpc_id = aws_vpc.simple_vpc.id
 
   tags = {
     Name = "simple_igw"
   }
 }
 
-# Создание таблицы маршрутов
-resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.vpc.id
+resource "aws_route_table" "simple_rt" {
+  vpc_id = aws_vpc.simple_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.simple_igw.id
   }
 
   tags = {
@@ -57,43 +53,36 @@ resource "aws_route_table" "rt" {
   }
 }
 
-# Ассоциация таблицы маршрутов с подсетью
-resource "aws_route_table_association" "rta" {
-  subnet_id      = aws_subnet.subnet.id
-  route_table_id = aws_route_table.rt.id
+resource "aws_route_table_association" "simple_rta" {
+  subnet_id      = aws_subnet.simple_subnet.id
+  route_table_id = aws_route_table.simple_rt.id
 }
 
-# Создание ECS кластера
-resource "aws_ecs_cluster" "cluster" {
+resource "aws_security_group" "simple_sg" {
+  vpc_id      = aws_vpc.simple_vpc.id
+  name        = "simple_sg"
+  description = "Allow HTTP traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_cluster" "simple_cluster" {
   name = "simple-cluster"
 }
 
-# Создание роли для задачи
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-# Присоединение политики к роли задачи
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-# Создание ECS задачи
-resource "aws_ecs_task_definition" "task" {
+resource "aws_ecs_task_definition" "simple_task" {
   family                   = "simple-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
@@ -107,8 +96,8 @@ resource "aws_ecs_task_definition" "task" {
       essential = true
       portMappings = [
         {
-          containerPort = 8080
-          hostPort      = 8080
+          containerPort = 80
+          hostPort      = 80
         }
       ]
       memory = 512
@@ -116,40 +105,72 @@ resource "aws_ecs_task_definition" "task" {
     }
   ])
 
-  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn = aws_iam_role.ecsTaskExecutionRole.arn
+  task_role_arn      = aws_iam_role.ecsTaskRole.arn
 }
 
-# Создание ECS сервиса
-resource "aws_ecs_service" "service" {
+resource "aws_ecs_service" "simple_service" {
   name            = "simple-service"
-  cluster         = aws_ecs_cluster.cluster.id
-  task_definition = aws_ecs_task_definition.task.arn
+  cluster         = aws_ecs_cluster.simple_cluster.id
+  task_definition = aws_ecs_task_definition.simple_task.arn
   desired_count   = 1
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets         = [aws_subnet.subnet.id]
-    security_groups = [aws_security_group.sg.id]
+    subnets         = [aws_subnet.simple_subnet.id]
+    security_groups = [aws_security_group.simple_sg.id]
   }
 }
 
-# Создание группы безопасности для ECS задачи
-resource "aws_security_group" "sg" {
-  name        = "allow-http"
-  description = "Allow HTTP inbound traffic"
-  vpc_id      = aws_vpc.vpc.id
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name = "ecsTaskExecutionRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
+  ]
+}
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "aws_iam_role" "ecsTaskRole" {
+  name = "ecsTaskRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  inline_policy {
+    name   = "ecsTaskPolicy"
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Effect = "Allow"
+          Action = [
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ]
+          Resource = "*"
+        }
+      ]
+    })
   }
 }
